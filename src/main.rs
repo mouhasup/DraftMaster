5defd5a3e556c1e27e84c2af7b62f984394a8e2a
mouhametadiouf <diegargonefaye@gmail.com>
2024-05-13 18:14:20 +0000

    fix main bugs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,183 @@
+extern crate sdl2;
+mod shapes;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use shapes::DmPoint;
+use std::time::Duration;
+use std::time::Instant;
+
+const WAIT_DURATION: Duration = Duration::from_millis(800);
+
 fn main() {
-    println!("Hello, let's draw!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 800)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut points: Vec<DmPoint> = Vec::new();
+    let mut anime = false;
+    let mut step = 0;
+    let mut last_event_time = Instant::now();
+    let mut old_point: Vec<DmPoint> = Vec::new();
+
+    'running: loop {
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            // Réinitialiser le temps du dernier événement
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::KpEnter),
+                    ..
+                } => {
+                    if points.len() > 1 {
+                        anime = true;
+                        let points_clone = &points;
+                        old_point = points_clone.clone();
+                        println!("len:{}", "points.len()");
+                    }
+
+                    // Ajoutez ici ce que vous voulez faire lors d'un clic gauche
+                }
+
+                Event::MouseButtonDown {
+                    mouse_btn, x, y, ..
+                } => {
+                    if mouse_btn == MouseButton::Left {
+                        if !anime {
+                            points.push(DmPoint::new(x, y));
+                            println!("len: {}", points.len());
+                        }
+                        // Ajoutez ici ce que vous voulez faire lors d'un clic gauche
+                    }
+                }
+
+                _ => {}
+            }
+        }
+
+        if anime {
+            if step == 7 {
+                // anime = false;
+                // points = Vec::new();
+                points = old_point.clone();
+                step = 0;
+            }
+
+            if Instant::now().duration_since(last_event_time) >= WAIT_DURATION {
+                last_event_time = Instant::now();
+                points = chaiks(&points);
+                step += 1;
+            }
+            canvas.set_draw_color(Color::WHITE);
+
+            if !points.is_empty() {
+                for k in 0..points.len() - 1 {
+                    let _ = canvas.draw_line(
+                        Point::new(points[k].x as i32, points[k].y as i32),
+                        Point::new(points[k + 1].x as i32, points[k + 1].y as i32),
+                    );
+                }
+            }
+            // The rest of the game loop goes here...
+        } else {
+            canvas.set_draw_color(Color::WHITE);
+            for k in 0..points.len() {
+                // let _ = canvas.draw_rect(Rect::new(
+                //     points[k].x as i32 - 3,
+                //     points[k].y as i32 - 3,
+                //     6,
+                //     6,
+                // ));
+                draw_circle(&mut canvas, points[k].x as i32, points[k].y as i32, 3)
+            }
+        }
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+fn chaiks(points: &Vec<DmPoint>) -> Vec<DmPoint> {
+    let mut new_points = Vec::new();
+    new_points.push(points[0]); // Ajouter le premier point
+
+    for i in 0..points.len() - 1 {
+        let p1 = points[i];
+        let p2 = points[i + 1];
+
+        // Ajouter les points intermédiaires
+        let q1 = p1.intermediate_point(p2, 1.0 / 4.0);
+        let q2 = p1.intermediate_point(p2, 3.0 / 4.0);
+
+        new_points.push(q1);
+        new_points.push(q2);
+    }
+
+    new_points.push(points[points.len() - 1]); // Ajouter le dernier point
+    new_points
+}
+
+fn draw_circle(canvas: &mut Canvas<Window>, center_x: i32, center_y: i32, radius: i32) {
+    let mut x = 0;
+    let mut y = radius;
+    let mut d = 3 - 2 * radius;
+
+    while y >= x {
+        draw_octant(canvas, center_x, center_y, x, y);
+        x += 1;
+        if d > 0 {
+            y -= 1;
+            d += 4 * (x - y) + 10;
+        } else {
+            d += 4 * x + 6;
+        }
+        draw_octant(canvas, center_x, center_y, x, y);
+    }
+}
+fn draw_octant(canvas: &mut Canvas<Window>, center_x: i32, center_y: i32, x: i32, y: i32) {
+    canvas
+        .draw_point(Point::new(center_x + x, center_y + y))
+        .unwrap();
+    canvas
+        .draw_point(Point::new(center_x - x, center_y + y))
+        .unwrap();
+    canvas
+        .draw_point(Point::new(center_x + x, center_y - y))
+        .unwrap();
+    canvas
+        .draw_point(Point::new(center_x - x, center_y - y))
+        .unwrap();
+    canvas
+        .draw_point(Point::new(center_x + y, center_y + x))
+        .unwrap();
+    canvas
+        .draw_point(Point::new(center_x - y, center_y + x))
+        .unwrap();
+    canvas
+        .draw_point(Point::new(center_x + y, center_y - x))
+        .unwrap();
+    canvas
+        .draw_point(Point::new(center_x - y, center_y - x))
+        .unwrap();
 }
--- src/shapes.rs
+++ src/shapes.rs
@@ -1,173 +1,82 @@
-pub struct Point {
+#[derive(Clone,Copy,Debug)]
+pub struct DmPoint {
     pub x: i32,
     pub y: i32,
 }
 
-pub struct Line {
-    pub start: Point,
-    pub end: Point,
+pub struct DmLine {
+    pub start: DmPoint,
+    pub end: DmPoint,
 }
-
-impl Point {
-/// Creates a new Point.
-///
-/// # Parameters
-/// - `x`: i32 representing the x-coordinate.
-/// - `y`: i32 representing the y-coordinate.
-///
-/// # Returns
-/// Returns a new Point instance with the specified x and y values.
-
-/// Calculates the distance from this point to another point.
-///
-/// # Parameters
-/// - `other`: the other Point to which distance is calculated.
-///
-/// # Returns
-/// Returns the Euclidean distance as a f64.
-
-
-
-/// Calculates the length from this point to a given line using perpendicular distance.
-///
-/// # Parameters
-/// - `line`: Line from which the distance will be calculated.
-///
-/// # Returns
-/// Returns the minimum distance as a f64.
-
-
-
-/// Calculates the intersection point of this line with another line if they intersect.
-///
-/// # Parameters
-/// - `line`: another Line to check intersection with.
-///
-/// # Returns
-/// Returns an Option<Point>. If the lines intersect, returns Some(Point) where the lines intersect; otherwise, None.
-
-
-
-/// Checks if this point lies on the given line.
-///
-/// # Parameters
-/// - `line`: Line to check if the point lies on.
-///
-/// # Returns
-/// Returns true if this point lies on `line`, false otherwise.
-
-
-
-/// Scales the point coordinates by a specified factor.
-///
-/// # Parameters
-/// - `factor`: f64 scaling factor for both x and y coordinates.
-///
-/// # Returns
-/// Returns a new Point with scaled coordinates.
-
-
-
-/// Rotates this point around another point by a given angle.
-///
-/// # Parameters
-/// - `center`: Point around which this point will be rotated.
-/// - `angle`: f64 representing the angle in radians to rotate.
-///
-/// # Returns
-/// Returns the new Point after rotation.
-
-
-
-/// Calculates the angle between this point and another point relative to the negative vertical direction.
-///
-/// # Parameters
-/// - `other`: another Point used to calculate the angle relative to this point.
-///
-/// # Returns
-/// Returns the angle in degrees with adjustments for negative trigonometric sense.
-
-
-
-/// Creates a new Line.
-///
-/// # Parameters
-/// - `start`: Point where the line starts.
-/// - `end`: Point where the line ends.
-///
-/// # Returns
-/// Returns a new Line instance with the specified start and end points.
-
-
-
-/// Computes the length of the line.
-///
-/// # Returns
-/// Returns the distance between the start and end points of the line as a f64.
-
-    
+impl DmPoint {
     pub fn new(x: i32, y: i32) -> Self {
-        Point { x, y }
+        DmPoint { x, y }
     }
-    pub fn distance_to(&self, other: Point) -> f64 {
+    pub fn intermediate_point(&self, p2: DmPoint, ratio: f32) -> DmPoint {
+        let x = ((1.0 - ratio) * self.x as f32 + ratio * p2.x as f32) as i32;
+        let y = ((1.0 - ratio) * self.y as f32 + ratio * p2.y as f32) as i32;
+        DmPoint { x, y }
+    }
+    pub fn distance_to(&self, other: DmPoint) -> f64 {
         (((self.x - other.x).pow(2) + (self.y - other.y).pow(2)) as f64).sqrt()
     }
-    pub fn length_from_line(&self, line: Line) -> f64 {
-        self.distance_to(self.intersection_point(line))
-    }
-    pub fn intersection_point(&self, line: Line) -> Option<Point> {
-        let Point { x: x1, y: y1 } = self.start;
-        let Point { x: x2, y: y2 } = self.end;
-        let Point { x: x3, y: y3 } = line.start;
-        let Point { x: x4, y: y4 } = line.end;
+    // pub fn length_from_line(&self, line: DmLine) -> f64 {
+    //     self.distance_to(self.intersection_point(line))
+    // }
+    // pub fn intersection_point(&self, line: DmLine) -> Option<DmPoint> {
+    //     let DmPoint { x: x1, y: y1 } = self.start;
+    //     let DmPoint { x: x2, y: y2 } = self.end;
+    //     let DmPoint { x: x3, y: y3 } = line.start;
+    //     let DmPoint { x: x4, y: y4 } = line.end;
 
-        let denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+    //     let denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
 
-        if denominator == 0 {
-            return None; // Lines are parallel or coincident
-        }
+    //     if denominator == 0 {
+    //         return None; // DmLines are parallel or coincident
+    //     }
 
-        let determinant1 = x1 * y2 - y1 * x2;
-        let determinant2 = x3 * y4 - y3 * x4;
+    //     let determinant1 = x1 * y2 - y1 * x2;
+    //     let determinant2 = x3 * y4 - y3 * x4;
 
-        let intersect_x =
-            ((determinant1 * (x3 - x4)) - (determinant2 * (x1 - x2))) as f64 / denominator as f64;
-        let intersect_y =
-            ((determinant1 * (y3 - y4)) - (determinant2 * (y1 - y2))) as f64 / denominator as f64;
+    //     let intersect_x =
+    //         ((determinant1 * (x3 - x4)) - (determinant2 * (x1 - x2))) as f64 / denominator as f64;
+    //     let intersect_y =
+    //         ((determinant1 * (y3 - y4)) - (determinant2 * (y1 - y2))) as f64 / denominator as f64;
 
-        Some(Point::new(
-            intersect_x.round() as i32,
-            intersect_y.round() as i32,
-        ))
-    }
-    pub fn is_in_line(&self, line: Line) -> bool {
-        self.intersection_point(line).map_or(false, |intersect| {
-            let line_length = self.length();
-            let start_to_intersect = self.start.distance_to(intersect);
-            let end_to_intersect = self.end.distance_to(intersect);
+    //     Some(DmPoint::new(
+    //         intersect_x.round() as i32,
+    //         intersect_y.round() as i32,
+    //     ))
+    // }
 
-            (start_to_intersect + end_to_intersect - line_length).abs() < f64::EPSILON
-        })
-    }
+    // pub fn is_in_line(&self, line: DmLine) -> bool {
+    //     self.intersection_point(line).map_or(false, |intersect| {
+    //         let line_length = self.length();
+    //         let start_to_intersect = self.start.distance_to(intersect);
+    //         let end_to_intersect = self.end.distance_to(intersect);
+
+    //         (start_to_intersect + end_to_intersect - line_length).abs() < f64::EPSILON
+    //     })
+    // }
     pub fn scale(&self, factor: f64) -> Self {
-        Point {
+        DmPoint {
             x: (self.x as f64 * factor) as i32,
             y: (self.y as f64 * factor) as i32,
         }
     }
-    pub fn rotate_around(&self, center: &Point, angle: f64) -> Self {
+    pub fn rotate_around(&self, center: &DmPoint, angle: f64) -> Self {
         let dx = self.x - center.x;
         let dy = self.y - center.y;
 
         let cos_angle = angle.cos();
         let sin_angle = angle.sin();
 
-        let new_x = (dx * cos_angle - dy * sin_angle) + center.x;
-        let new_y = (dx * sin_angle + dy * cos_angle) + center.y;
+        let new_x = (dx as f64 * cos_angle - dy as f64 * sin_angle) + center.x as f64;
+        let new_y = (dx as f64 * sin_angle + dy as f64 * cos_angle) + center.y as f64;
 
-        Point::new(new_x as i32, new_y as i32)
+        DmPoint::new(new_x as i32, new_y as i32)
     }
-    pub fn angle_to_vertical_neg(&self, other: &Point) -> f64 {
+    pub fn angle_to_vertical_neg(&self, other: &DmPoint) -> f64 {
         let dx = self.x - other.x;
         let dy = self.y - other.y;
 
@@ -178,10 +87,10 @@ impl Point {
         let vertical_length = 1.0;
 
         // Utiliser la formule de l'angle entre deux vecteurs pour calculer l'angle
-        let angle = (dx / vector_length).atan2(dy / vector_length);
+        let angle = (dx as f64 / vector_length).atan2(dy as f64 / vector_length);
 
         // Convertir l'angle en degrés et ajuster pour le sens trigonométrique négatif
-        let adjusted_angle = angle.to_degrees();
+        let mut adjusted_angle = angle.to_degrees();
         if adjusted_angle < 0.0 {
             adjusted_angle += 360.0; // Ajouter 360 degrés pour ajuster dans le sens trigonométrique négatif
         }
@@ -189,9 +98,9 @@ impl Point {
     }
 }
 
-impl Line {
-    pub fn new(start: Point, end: Point) -> Self {
-        Line { start, end }
+impl DmLine {
+    pub fn new(start: DmPoint, end: DmPoint) -> Self {
+        DmLine { start, end }
     }
 
     pub fn length(&self) -> f64 {
